f952fa862779f365bb0af25f6283bd6609bb3e38
RSiniavski <r.siniavski@outlook.com>
2019-04-25 17:10:20 +0300

    some fix

--- browser_settings.py
+++ browser_settings.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 CHROME_VERSION = '73'
 
-# driver = webdriver.Chrome(f'E:/PythonProjects/CLAP---tests/chromedriver{CHROME_VERSION}.exe')
-driver = webdriver.Remote(
-    command_executor='http://127.0.0.1:4444/wd/hub',
-    desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+driver = webdriver.Chrome(f'E:/PythonProjects/CLAP---tests/chromedriver{CHROME_VERSION}.exe')
+# driver = webdriver.Remote(
+#     command_executor='http://127.0.0.1:4444/wd/hub',
+#     desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
